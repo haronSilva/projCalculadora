c8dfc19c305745f5fa50d5607c26ce275f250d0e
haron.costa <haron.costa@softvaro.com.br>
2019-03-01 15:01:34 -0300

    include swagger document

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const app = express()
 require('../src/controller/calculoTrabalhista')(app)
 require('../src/controller/calculoMatematico')(app)
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
+const bodyParser = require("body-parser")
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(bodyParser.json())
 app.listen(porta,() =>{
     console.log(`Server ativo na porta ${porta} `)
 })
